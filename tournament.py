9b6221749bac19a8883e5f2dcf0be9e7d20753b6
rubendarioramirez <ramirez.ruben.dario10@gmail.com>
2015-03-11 23:13:10 +0700

    Initial commit
    Connect to database working.
    Count player working.
    Register player working.
    Delete players working.

---
+++ tournament.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# 
+# tournament.py -- implementation of a Swiss-system tournament
+#
+
+import psycopg2
+
+
+def connect():
+    """Connect to the PostgreSQL database.  Returns a database connection."""
+    return psycopg2.connect("dbname=tournament")
+
+
+def deleteMatches():
+    """Remove all the match records from the database."""
+
+
+def deletePlayers():
+    """Remove all the player records from the database."""
+    #Connect to database
+    db = connect()
+    #create the cursor
+    c = db.cursor()
+    #execute the query
+    c.execute("DELETE FROM players")
+    #commit delete if all goes fine
+    db.commit()
+    #close the database since i dont need it anymore
+    db.close()
+
+def countPlayers():
+    """Returns the number of players currently registered."""
+    #Connect to database
+    db = connect()
+    #create the cursor
+    c = db.cursor()
+    #execute the query
+    c.execute("SELECT COUNT(name) FROM players")
+    #store the results in a variable named result
+    results = c.fetchone()[0]
+    #close the database since i dont need it anymore
+    db.close()
+
+    return results
+    
+def registerPlayer(name):
+    """Adds a player to the tournament database.
+  
+    The database assigns a unique serial id number for the player.  (This
+    should be handled by your SQL database schema, not in your Python code.)
+  
+    Args:
+      name: the player's full name (need not be unique).
+    """
+    #Connect to database
+    db = connect()
+    #create the cursor
+    c = db.cursor()
+    #execute the query - The %s means save this space for the variable, and then i definy the variable outside
+    c.execute("INSERT INTO players (name) VALUES (%s)", (name,))
+    #commit if all goes fine
+    db.commit()
+    #close the database since i dont need it anymore
+    db.close()
+
+def playerStandings():
+    """Returns a list of the players and their win records, sorted by wins.
+
+    The first entry in the list should be the player in first place, or a player
+    tied for first place if there is currently a tie.
+
+    Returns:
+      A list of tuples, each of which contains (id, name, wins, matches):
+        id: the player's unique id (assigned by the database)
+        name: the player's full name (as registered)
+        wins: the number of matches the player has won
+        matches: the number of matches the player has played
+    """
+
+
+def reportMatch(winner, loser):
+    """Records the outcome of a single match between two players.
+
+    Args:
+      winner:  the id number of the player who won
+      loser:  the id number of the player who lost
+    """
+ 
+ 
+def swissPairings():
+    """Returns a list of pairs of players for the next round of a match.
+  
+    Assuming that there are an even number of players registered, each player
+    appears exactly once in the pairings.  Each player is paired with another
+    player with an equal or nearly-equal win record, that is, a player adjacent
+    to him or her in the standings.
+  
+    Returns:
+      A list of tuples, each of which contains (id1, name1, id2, name2)
+        id1: the first player's unique id
+        name1: the first player's name
+        id2: the second player's unique id
+        name2: the second player's name
+    """
+
+
---
+++ tournament.sql
@@ -0,0 +1,14 @@
+-- Table definitions for the tournament project.
+--
+-- Put your SQL 'create table' statements in this file; also 'create view'
+-- statements if you choose to use it.
+--
+-- You can write comments in this file by starting them with two dashes, like
+-- these lines here.
+
+-- Create database
+CREATE DATABASE tournament;
+
+--CREATE TABLES
+CREATE TABLE players (id serial primary key, name text)
+
